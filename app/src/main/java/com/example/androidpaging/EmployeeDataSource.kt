10861ae0bf75a549305104051366a9d9fefa0a2c
techtrixbangladesh <techtrixbangladesh@gmail.com>
2019-07-03 18:39:54 +0600

    Commit from Office

--- app/src/main/java/com/example/androidpaging/EmployeeDataSource.kt
+++ app/src/main/java/com/example/androidpaging/EmployeeDataSource.kt
@@ -1,25 +1,20 @@
 package com.example.androidpaging
 
 import androidx.paging.ItemKeyedDataSource
-import com.example.androidpaging.retrofit.ApiInterface
 import com.example.androidpaging.retrofit.RetroFitApiClient
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import android.provider.Contacts.Photos
-import android.R.attr.data
 import com.example.androidpaging.model.ModelEmployee
-import io.reactivex.Single
-import android.R.attr.data
-import android.widget.Toast
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
-import okhttp3.ResponseBody
 
 
 class EmployeeDataSource: ItemKeyedDataSource<Any, Any>() {
     override fun loadInitial(params: LoadInitialParams<Any>, callback: LoadInitialCallback<Any>) {
-        val apiInterface : ApiInterface = RetroFitApiClient.instance
-        val question = apiInterface.getQuestion();
+
+        val instance:RetroFitApiClient = RetroFitApiClient.instance
+        val question = instance.getQuestion()
+
         question.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(object :SingleObserver<List<ModelEmployee>> {
@@ -42,7 +37,25 @@ class EmployeeDataSource: ItemKeyedDataSource<Any, Any>() {
     }
 
     override fun loadAfter(params: LoadParams<Any>, callback: LoadCallback<Any>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val instance = RetroFitApiClient.instance
+        val question = instance.getQuestion()
+        question.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object :SingleObserver<List<ModelEmployee>> {
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onSuccess(responseBody: List<ModelEmployee>) {
+                    callback.onResult(responseBody);
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+            })
     }
 
     override fun loadBefore(params: LoadParams<Any>, callback: LoadCallback<Any>) {
--- app/src/main/java/com/example/androidpaging/retrofit/RetroFitApiClient.kt
+++ app/src/main/java/com/example/androidpaging/retrofit/RetroFitApiClient.kt
@@ -1,17 +1,24 @@
 package com.example.androidpaging.retrofit
 
+import com.example.androidpaging.model.ModelEmployee
+import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.http.GET
 
-class RetroFitApiClient {
+interface RetroFitApiClient {
+
+    @GET("api/v1/employees")
+    fun getQuestion(): Single<List<ModelEmployee>>
+
 
     companion object {
-        val instance: ApiInterface by lazy {
+        val instance: RetroFitApiClient by lazy {
             val retrofit = Retrofit.Builder()
                 .baseUrl("http://dummy.restapiexample.com/")
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
-            retrofit.create(ApiInterface::class.java)
+            retrofit.create(RetroFitApiClient::class.java)
         }
     }
 }
